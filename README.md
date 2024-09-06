5499da55b1e66fc8f4a3fc2e6dba298e889721b2
jiyamittal25 <jiyamittal25@gmail.com>
2024-09-06 17:13:30 +0530

    morechanges3

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 Namaste React
+
+#Parcel
+-Dev Build
+-Local Server
+-HMR - Hot Module Replacement
+-File Watching Algorithm - written in c++ 
+-Caching - Faster Builds 
+-Image Optimization
+-Minification 
+-Bundling
+-Compress
+-Consistent Hashing - support older browsers also
+-Diagnostic
+-Error Handling
+-HTTPs
+-Tree Shaking - remove unused code
+- Different  deva nd prod bundles
--- app.js
+++ app.js
@@ -24,60 +24,83 @@
 // const root=ReactDOM.createRoot(document.getElementById("root"));
 // root.render(parent);
 
-const parent=React.createElement(
-    "div",
-    {id:"parent"},
-    [
-        React.createElement(
-            "div",
-            {id:"child1"},
-            [
-                React.createElement(
-                    "h1",
-                    {},
-                    "I'm an h1 tag of child1 tag"
-                ),
-                React.createElement(
-                    "h2",
-                    {},
-                    "I'm an h2 tag of child1 tag"
-                )
-            ]
-        ),
-        React.createElement(
-            "div",
-            {id:"child2"},
-            [
-                React.createElement(
-                    "h1",
-                    {},
-                    "I'm an h1 tag of child2 tag"
-                ),
-                React.createElement(
-                    "h2",
-                    {},
-                    "I'm an h2 tag of child2 tag"
-                )
-            ]
-        )
-    ]
-);
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// const parent=React.createElement(
+//     "div",
+//     {id:"parent"},
+//     [
+//         React.createElement(
+//             "div",
+//             {id:"child1"},
+//             [
+//                 React.createElement(
+//                     "h1",
+//                     {},
+//                     "I'm an h1 tag of child1 tag of namaste react"
+//                 ),
+//                 React.createElement(
+//                     "h2",
+//                     {},
+//                     "I'm an h2 tag of child1 tag"
+//                 )
+//             ]
+//         ),
+//         React.createElement(
+//             "div",
+//             {id:"child2"},
+//             [
+//                 React.createElement(
+//                     "h1",
+//                     {},
+//                     "I'm an h1 tag of child2 tag"
+//                 ),
+//                 React.createElement(
+//                     "h2",
+//                     {},
+//                     "I'm an h2 tag of child2 tag"
+//                 )
+//             ]
+//         )
+//     ]
+// );
+
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+
+// /* <div id="parent">
+//     <div id="child1">
+//         <h1></h1>
+//         <h2></h2>
+//     </div>
+//     <div id="child2">
+//         <h1></h1>
+//         <h2></h2>
+//     </div>
+// </div>
+
+// ReactElement(object)=>HTML(Browser understand)
+
+// */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { jsx } from "react/jsx-runtime";
+
+// React.createElement => ReactElement-JS Object => HTMLElement(render)
+
+// const heading= React.createElement("h1",{id:"heading"},"Namaste React");
+// console.log(heading);
+
+// JSX- HTML-like or XML-like syntax ,  It transpiled(converted to browser TF5F6DED4dde4p-----) before it reaches the JS - PARCEL - Babel
+// JSX => React.createElement => ReactElement-JS Oject => HTMLElement(render)
+const jsxHeading= <h1 id="heading">Namaste React using JSX</h1>
+console.log(jsxHeading);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
+root.render(jsxHeading);
 
 
-/* <div id="parent">
-    <div id="child1">
-        <h1></h1>
-        <h2></h2>
-    </div>
-    <div id="child2">
-        <h1></h1>
-        <h2></h2>
-    </div>
-</div>
-
-ReactElement(object)=>HTML(Browser understand)
-
-*/
