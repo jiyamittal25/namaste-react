6363bd680e7b58af48128cbf42115813fe35d823
jiyamittal25 <jiyamittal25@gmail.com>
2024-09-06 19:28:30 +0530

    more changes 6

--- app.js
+++ app.js
@@ -87,7 +87,6 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { jsx } from "react/jsx-runtime";
 
 // React.createElement => ReactElement-JS Object => HTMLElement(render)
 
